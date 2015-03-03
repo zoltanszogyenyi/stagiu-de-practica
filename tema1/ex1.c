201994eb5190193353ebc2c584094c1529b72e30
zoltanszogyenyi <zoltan.szogyenyi@gmail.com>
2015-03-03 17:32:58 +0200

    added description

--- tema1/ex1.c
+++ tema1/ex1.c
@@ -1,3 +1,14 @@
+/*
+
+Pentru un tablou cu cel mult 50 de elemente de tip real, citit de la tastatura, sa se construiasca un nou tablou in urmatorul mod: intre doua elemente consecutive se insereaza maximul lor in valoare absoluta, daca diferența lor este strict mai mica decat 10. 
+Ex:pentru n = 5, si (-20,-5,15,-13,14) =>(-20,20,-5,15,15,-13,14,14).
+Obs. Se rezolva in doua moduri:
+
+a) cu un tabou ajutator 
+b) fara un tablou ajutator.
+
+*/
+
 #include <stdio.h>
 
 int main()
