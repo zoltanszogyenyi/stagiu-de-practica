cfa6cc714ffe9a93c42dd6ed9a3faf42c2366706
zoltanszogyenyi <zoltan.szogyenyi@gmail.com>
2015-03-05 21:08:44 +0200

    Added the exercise. This was very simple.

---
+++ tema1/ex8.c
@@ -0,0 +1,51 @@
+/*
+
+Student:              Szogyenyi Zoltan
+Anul:                 I
+Materie:              Stagiu de Practica
+Limbaj de programare: C
+Data:                 Martie 3 2015 - Marti
+
+7.) Exercitiul:
+
+Se considera un polinom cu coeficienti reali de 2 variabile x si y.
+Sa se implementeze functii pentru adunarea, scaderea si inmultirea
+polinoamelor de acest tip.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+	// coeficientii polinoamelor
+	int a, b, c, d;
+	
+	printf("Coeficintul a\n");
+	scanf("%d", &a);
+	printf("Coeficintul b\n");
+	scanf("%d", &b);
+
+	printf("%dx + %dy\n", a, b);
+
+	printf("Coeficintul c\n");
+	scanf("%d", &c);
+	printf("Coeficintul d\n");
+	scanf("%d", &d);
+
+	printf("%dx + %dy\n", c, d);
+
+	printf("Operatiunile de adaugare, scadere si inmultire:\n");
+
+	// adunarea polinoamelor
+	printf("Adunarea: %dx + %dy\n", a+c, b+d);
+
+	// scaderea polinoamelor
+	printf("Scaderea: %dx + %dy\n", a-c, b-d);
+
+	// inmultirea polinoamelor
+	printf("Inmultirea: %dx^2 + %dy^2\n", a*c, b*d);
+
+	printf("Exit...\n");
+
+	return 0;
+}
